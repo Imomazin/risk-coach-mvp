29df431f36725ab6bc698a2902069bd8b14286b0
Imomazin <imomazin.ae@gmail.com>
2026-01-26 17:50:21 +0000

    Remove BrowserRouter wrapper from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import CoverPage from "./pages/CoverPage";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
@@ -6,25 +7,23 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* PUBLIC ROUTES */}
-        <Route path="/" element={<CoverPage />} />
-        <Route path="/login" element={<LoginPage />} />
+    <Routes>
+      {/* Public */}
+      <Route path="/" element={<CoverPage />} />
+      <Route path="/login" element={<LoginPage />} />
 
-        {/* PROTECTED ROUTES */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+      {/* Protected */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
 
-        {/* FALLBACK */}
-        <Route path="*" element={<CoverPage />} />
-      </Routes>
-    </BrowserRouter>
+      {/* Fallback */}
+      <Route path="*" element={<CoverPage />} />
+    </Routes>
   );
 }
